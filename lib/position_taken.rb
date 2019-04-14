5e02b84d57d20efe3b6034a2a7af5eea2d0e9ec7
Lisa <lisaantwi@gmail.com>
2019-04-14 06:28:58 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,7 @@
 def position_taken?(board, index)
   if (board[index] == " " || board[index] == "" || board[index] == nil)
     return FALSE
-    elsif (board[index] == X || )
+    elsif (board[index] == X || board[index] == O ||  )
+    return true 
   end
 end
